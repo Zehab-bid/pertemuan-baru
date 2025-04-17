debb6172d297b159c97abba836b869adc9f3ddff
Zehab-bid <zehabidha152gmail.com>
2025-04-17 18:27:41 +0700

    proges 2

--- mergesort.cpp
+++ mergesort.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 int arr[20], B[20]; // Array utama dan array bantu
 int n;
+
+void input()-
+
+void mergeSort(int low,int high){
+    if (low >= high)
+    {               // step 1
+        return;     // step 1a
+    }
+
+    int mid = (low + high) / 2; // step 2
+
+    mergeSort(low, mid); //step 3a
+    mergeSort(mid + 1, high);
+}
