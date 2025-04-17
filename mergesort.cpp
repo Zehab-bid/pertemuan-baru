bebd8194bcd7ea79883363ff4c422c0b23bc851b
Zehab-bid <zehabidha152gmail.com>
2025-04-17 18:39:28 +0700

    proges 3

--- mergesort.cpp
+++ mergesort.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int arr[20], B[20]; // Array utama dan array bantu
 int n;
 
-void input()-
 
 void mergeSort(int low,int high){
     if (low >= high)
@@ -14,6 +13,27 @@ void mergeSort(int low,int high){
 
     int mid = (low + high) / 2; // step 2
 
-    mergeSort(low, mid); //step 3a
-    mergeSort(mid + 1, high);
+    mergeSort(low, mid);      //step 3a
+    mergeSort(mid + 1, high); // step 3b
+
+    int i = low; // step 4a
+    int j, k;
+    j = mid + 1; // step 4b
+    k = low;   // step 4c
+
+    while (i <= mid && j <= high) // step 4d
+    {
+        if (arr[i] <= arr[j]) // step 4.d.i
+        {
+            B[k] = arr[i]; // step 4.d.i.1
+            i++;            // step 4.d.i.2
+        }
+        else
+        {
+            B[k] = arr[j]; // step 4.d.i.3
+            j++;            // step 4.d.i.4
+        }
+    }
+
+
 }
